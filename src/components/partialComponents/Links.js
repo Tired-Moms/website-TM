38ac64398d6fd6f5f4d2779268d64a21d87ca37f
julmo <julmo611@gmail.com>
2022-01-12 15:53:15 -0800

    links update

--- src/components/partialComponents/Links.js
+++ src/components/partialComponents/Links.js
@@ -6,18 +6,26 @@ import opensea from '../../assets/opensea-logo.png';
 export default function Links() {
 	return (
 		<ul className="navLinks">
-			<li>
+			{/* <li>
 				<a href="#" target="_blank" rel="noopener noreferrer">
 					<img src={opensea} alt="Opensea icon" />
 				</a>
-			</li>
+			</li> */}
 			<li>
-				<a href="#" target="_blank" rel="noopener noreferrer">
+				<a
+					href="https://discord.io/tiredmomsnft"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
 					<img src={discord} alt="Discord icon" />
 				</a>
 			</li>
 			<li>
-				<a href="#" target="_blank" rel="noopener noreferrer">
+				<a
+					href="https://twitter.com/tiredmomsnft"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
 					<img src={twitter} alt="Twitter icon" />
 				</a>
 			</li>
