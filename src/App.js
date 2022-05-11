33df5c6c03726c0b6b2e4860a614ac35c2d2f47e
julmo <julmo611@gmail.com>
2022-05-10 21:51:37 -0700

    Fixing GTM

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import Homepage from './components/Homepage';
 import Footer from './components/partialComponents/Footer';
 import Navigation from './components/partialComponents/Navigation';
+import TagManager from 'react-gtm-module';
 
 function App() {
+	const tagManagerArgs = {
+		gtmId: 'G-7HP2G31L9C',
+	};
+	TagManager.initialize(tagManagerArgs);
 	return (
 		<div className="mainContainer">
 			<Navigation />
